66d398461a2f4222753700636315e1e317c3fa95
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-01-22 16:48:13 -0500

    functional: provide equivalent alternative

--- src/formallang/functional.clj
+++ src/formallang/functional.clj
@@ -3,4 +3,5 @@
 (defn mmap
   "Returns a map {k (f v)} for all (k v) pairs in m."
   [f m]
+  ; equivalently, (reduce (fn [acc cur] (update acc cur f)) m (keys m))
   (into (empty m) (for [[k v] m] [k (f v)])))
